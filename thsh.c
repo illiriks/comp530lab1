c9b1d3f4b889de1d5b53e85edd33cfb97c1e07f3
Illirik Smirnov <ims@live.unc.edu>
2016-09-28 23:11:46 -0400

    Add files via upload

--- thsh.c
+++ thsh.c
@@ -4,32 +4,62 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
 // Assume no input line will be longer than 1024 bytes
 #define MAX_INPUT 1024
 
-void execute(char ** argv){
+
+bool is_empty(char s[]) {
+	int k=0;
+	while (s[k] != '\0') {
+	  if (!isspace(*s)) return false;
+		k++;
+	}
+	return true;
+}
+
+//Executes a command. Program is in argv[0], args are in argv.
+void execute(char ** argv, bool debug){
 	int pid;
 	int status;
-
+	// Fork process
 	pid = fork();
 	if (pid == 0) { // Child
+		// Execute child
 		if (execvp(*argv, argv) < 0){
-			printf("%s", *argv);
 			exit(1);
 		}
 	} else {
+		if (debug){
+			printf("RUNNING: %s\n", argv[0]);
+		}
+		// Parent process should 'sleep' while child isn't done 
 		while (wait(&status) != pid);
+		// Child is done here
+		if (debug){
+			printf("ENDED: %s (ret=%d)\n", argv[0], status);
+		}
 	}
 }
 
+// Converts a string to an array of strings, tokenized
 char ** parse(char * string) {
-	char ** returnme = malloc(sizeof(char*) * MAX_INPUT / 2);
+	// I don't know how to allocate 2d string arrays in C
+	// but this works
+	// although my laptop has 16GB RAM so it's OK
+	char** returnme= malloc(1000000); 
   int current = 0;
 	const char s[2] = " ";
 	char *token;
 	token = strtok(string, s);
+	// in case of empty string
+	// while there is more string to tokenize,
 	while (token != NULL ) {
-		*(returnme + current) = token;
+		// add the token to the array and move forward
+		if (token[0] == '$')
+			token = getenv(token+1);			
+		returnme[current] = token;
 		token = strtok(NULL, s);
 		current ++;
 	}
@@ -41,8 +71,13 @@ main (int argc, char ** argv, char **envp) {
   int finished = 0;
   char *prompt = "thsh> ";
   char cmd[MAX_INPUT];
-
-
+	char cwd[1024];
+	bool debug = false;
+	// if the -d argument is passed, run this code in debug mode
+	if ((argc > 1) && (strcmp(argv[1], "-d") == 0)){
+			debug = true;
+	}
+	// main loop
   while (!finished) {
     char *cursor;
     char last_char;
@@ -51,7 +86,11 @@ main (int argc, char ** argv, char **envp) {
 
 
     // Print the prompt
-    rv = write(1, prompt, strlen(prompt));
+		write(1, "[", 2);
+		// print cwd in prompt
+    write(1, getcwd(cwd, sizeof(cwd)), strlen(getcwd(cwd, sizeof(cwd))));
+		write(1, "] ", 3);
+		rv = write(1, prompt, strlen(prompt));
     if (!rv) { 
       finished = 1;
       break;
@@ -69,20 +108,26 @@ main (int argc, char ** argv, char **envp) {
       last_char = *cursor;
     } 
 	 	*(cursor-1) = '\0';
-
+		// if read fails
     if (!rv) { 
       finished = 1;
       break;
     }
-
-		char ** parsed_command = parse(cmd);
-		if (strcmp(*parsed_command, "exit") == 0){
-			exit(3);
+		// put builtin commands here
+		if (!is_empty(cmd)){
+			char ** parsed_command = parse(cmd);
+			if (strcmp(parsed_command[0], "exit") == 0)
+				exit(3);
+			else if (strcmp(parsed_command[0], "cd") == 0)
+				// THIS ONLY WORKS IF A PATH IS SPECIFIED
+				// 'cd -' and 'cd' ALONE DO NOT WORK
+				chdir(parsed_command[1]);
+			else if (strcmp(parsed_command[0], "set") == 0){
+				//TODO: FIX THIS
+				printf("%d\n", putenv(parsed_command[1]));
+			}
+			else execute(parsed_command, debug);
 		}
-    // Execute the command, handling built-in commands separately 
-    // Just echo the command line for now
-		execute(parsed_command);
-    //write(1, cmd, strnlen(cmd, MAX_INPUT));
 
   }
 
